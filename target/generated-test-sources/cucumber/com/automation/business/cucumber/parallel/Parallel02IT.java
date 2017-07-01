e4094b331d25977b2a17e43e1d74db0a438d0d15
Mridul J Kurup <mriduljkurup@gmail.com>
2017-07-01 00:59:07 -0500

    Made changes to include cucumber jvm parallel plugin. Doesn't seem to work

---
+++ target/generated-test-sources/cucumber/com/automation/business/cucumber/parallel/Parallel02IT.java
@@ -0,0 +1,16 @@
+package com.automation.business.cucumber.parallel;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        strict = true,
+        features = {"/home/stealth17/IdeaProjects/cucumberpoc2/src/main/resources/features/search2.feature:11"},
+        plugin = {"json:/home/stealth17/IdeaProjects/cucumberpoc2/target/cucumber-parallel/2.json"},
+        monochrome = true,
+        glue = {"com.automation.business.cucumber.stepdefinitions"})
+public class Parallel02IT {
+}
